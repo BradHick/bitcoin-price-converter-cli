3d579cc0bdbe1fecbba9d2f908dad5cc85464750
BradHick <bradleyhrm@gmail.com>
2018-10-17 13:41:10 -0300

    refactor: change help in a single file

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 const https = require('https');
-const showAllCurrencies = require('./show-all-currency')
+const showAllCurrencies = require('./show-all-currency');
+const help = require('./help');
 
 const arguments = process.argv.splice(2, process.argv.length -1).join(' ')
 const argArray = arguments.split(' ');
@@ -38,46 +39,7 @@ if(!arguments){
 }
 
 if(arguments === '--help'){
-  console.log('=========== Help bitcoin-price-converter =============');
-  console.log();
-  console.log('Parameters: [Currency] [Value]');
-  console.log('--- Currency: Currency abbreviation');
-  console.log('--- Value: Amount');
-  console.log('--- Eg: bitcoin BRL 1000');
-  console.log();
-  console.log('If you only enter "bitcoin" in the terminal, a list with all the supported currencies and their details will be shown');
-  console.log();
-  console.log('By entering only "bitcoin [Currency]" in the terminal, the details of that currency will be shown');
-  console.log('----Ed: bitcoin BRL');
-  console.log();
-  console.log('Use . (dot) for values ​​in currency subunits');
-  console.log('----Ed: bitcoin BRL 571.75');
-  console.log();
-  console.log('====================================');
-  console.log('List of supported currencies: ');
-  console.log('USD - Dollar');
-  console.log('AUD - Australian dollar');
-  console.log('BRL - Brazilian Real');
-  console.log('CAD - Canadian dollar');
-  console.log('CLP - Chilean peso');
-  console.log('CNY - China Yuan / Renminbi');
-  console.log('DKK - Danish krone');
-  console.log('EUR - Euro');
-  console.log('GBP - Pound sterling');
-  console.log('HKD - Dolar Honkongense');
-  console.log('INR - India Rupee');
-  console.log('ISK - Iraqi Dinar');
-  console.log('JPY - Japanese yen');
-  console.log('KRW - Won South Korean');
-  console.log('NZD - New Zealand Dollar');
-  console.log('PLN - Polish Zloty');
-  console.log('RUB - Russian Ruble');
-  console.log('SEK - Swedish krona');
-  console.log('SGD - Singaporean dollar');
-  console.log('THB - Thai Baht');
-  console.log('TWD - Taiwanese dollar');
-  console.log('====================================');
-  
+  return help();
 };
 
 if(translateCurrency[currency.toUpperCase()]){
